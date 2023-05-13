6c0216838778c303b3a1709100a71c5cc4a79014
Youssef-Amir98 <youssefamir@aucegypt.edu>
2023-05-13 12:20:56 +0300

    include name in card controller and jbuilder

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -65,6 +65,6 @@ class CardsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def card_params
-      params.require(:card).permit(:list_id, :position)
+      params.require(:card).permit(:list_id, :name, :position)
     end
 end
