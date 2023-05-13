5bd75878b5263ad81d05c9d3dd9963e07426d179
Youssef-Amir98 <youssefamir@aucegypt.edu>
2023-05-13 12:22:21 +0300

    now including rails ajx for adding cards

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,19 +1,51 @@
 <template>
   <div id="app" class="row">
-    <div v-for="list in original_lists" class="col-3">
+    <div v-for="list in lists" class="col-3">
       <h6>{{ list.name }}</h6>
       <hr />
 
-      <div v-for="(card,index) in list.cards" class="card card-body">
+      <div v-for="(card,index) in list.cards" class="card card-body mt-3">
         {{ card.name }}
+      </div>
+
+      <div class="card card-body mt-3">
+        <textarea v-model="messages[list.id]" class="form-control"></textarea>
+        <button v-on:click="submitMessges(list.id)" class="btn btn-secondary">Add</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Rails from "@rails/ujs"
+
 export default {
   props: ["original_lists"],
+  data: function(){
+    return {
+      messages: {},
+      lists: this.original_lists
+    }
+  },
+  methods: {
+    submitMessges: function(list_id){
+      var data = new FormData
+      data.append("card[list_id]", list_id)
+      data.append("card[name]", this.messages[list_id])
+
+      Rails.ajax({
+        url: "/cards",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: (data) => {
+          const index = this.lists.findIndex(item => item.id == list_id)
+          this.lists[index].cards.push(data)
+          this.messages[list_id] = undefined
+        }
+      })
+    }
+  }
 }
 </script>
 
