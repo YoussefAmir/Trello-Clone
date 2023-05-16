fc50451d322247161e9316ff6d841cbfbef5057b
Youssef-Amir98 <youssefamir@aucegypt.edu>
2023-05-16 11:42:16 +0300

    using vue components

--- app/javascript/components/board.vue
+++ app/javascript/components/board.vue
@@ -1,6 +1,13 @@
 <template>
   <draggable v-model="lists" group="lists" class="board dragArea" @end="listMoved">
     <list v-for="list in lists" :list="list"></list>
+
+    <div class="list">
+      <a v-if="!editing" v-on:click="startEditing">Add a list</a>
+      <textarea v-if="editing" ref="message" v-model="message" class="form-control mb-2"></textarea>
+      <button v-if="editing" v-on:click="createList" class="btn btn-secondary me-1">Add</button>
+      <a v-if="editing" v-on:click="editing=false">Cancel</a>
+    </div>
   </draggable>
 </template>
 
@@ -11,11 +18,20 @@ import list from "../components/list.vue"
 
 export default {
   components: { draggable: VueDraggableNext, list },
-  props: ["original_lists"],
   data: function(){
     return {
-      messages: {},
-      lists: this.original_lists
+      editing: false,
+      message: ""
+    }
+  },
+  computed: {
+    lists: {
+      get() {
+        return store.state.lists
+      },
+      set(value) {
+        store.state.lists = value
+      }
     }
   },
   methods: {
@@ -28,6 +44,26 @@ export default {
         type: "PATCH",
         data: data,
         dataType: "json"
+      })
+    },
+    startEditing: function() {
+      this.editing = true
+      this.$nextTick(() => { this.$refs.message.focus() })
+    },
+    createList: function(){
+      var data = new FormData
+      data.append("list[name]", this.message)
+
+      Rails.ajax({
+          url: "/lists",
+          type: "POST",
+          data: data,
+          dataType: "json",
+          success: (data) => {
+            store.commit("addList", data)
+            this.message = ""
+            this.editing = false
+          }
       })
     }
   }
@@ -42,5 +78,14 @@ export default {
 .board {
   white-space: nowrap;
   overflow-x: auto;
+}
+.list {
+  background: lightgray;
+  border-radius: 3px;
+  width: 270px;
+  vertical-align: top;
+  margin-right: 20px;
+  padding: 20px;
+  display: inline-block;
 }
 </style>
--- app/javascript/components/card.vue
+++ app/javascript/components/card.vue
@@ -1,15 +1,75 @@
 <template>
-    <div class="card card-body mt-3">
-        {{ card.name }}
+    <div>
+        <div @click="editing=true" class="card card-body mt-3">
+            {{ card.name }}
+        </div>
+
+        <div v-if='editing' class="modal-backdrop show"></div>
+
+        <div v-if='editing' @click="closeModal" class="modal show" style="display: block">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">{{ card.name }}</h5>
+            </div>
+            <div class="modal-body">
+                <input v-model="card.name" class="form-control">
+            </div>
+            <div class="modal-footer">
+                <button @click="save" type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+        </div>
     </div>
 </template>
 
 <script>
+import Rails from "@rails/ujs"
+import { storeKey } from "vuex"
+
 export default{
-    props: [ "card" ],
+    props: [ "card"],
     data: function() {
         return {
-            editing: false
+            editing: false,
+        }
+    },
+    // emits: { 
+    //     updateCard: function(newValue){
+    //         return newValue
+    //     }
+    // },
+    // computed: {
+    //     card: {
+    //         get() {
+    //             return this.cardModel
+    //         },
+
+    //         set(newValue) {
+    //             this.cardModel = newValue
+    //         }
+    //     }
+
+    // },
+    methods: {
+        closeModal: function(){
+            if (event.target.classList.contains("modal")) { this.editing = false }
+        },
+        save: function(){
+            var data = new FormData
+            data.append("card[name]", this.card.name)
+
+            Rails.ajax({
+                url: `/cards/${this.card.id}`,
+                type: "PATCH",
+                data: data,
+                dataType: "json",
+                success: (data) => {
+                    store.commit("editCard", data)
+                    this.editing = false
+                }
+            })
         }
     }
 }
